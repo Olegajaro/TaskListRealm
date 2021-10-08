8605cb7ff2d81e81880023cbdaf5ad9b854bcda1
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-10-08 22:20:35 +0300

    Implemented displaying data from the database in tabular views

--- TaskListRealm/ViewControllers/TaskListViewController.swift
+++ TaskListRealm/ViewControllers/TaskListViewController.swift
@@ -5,26 +5,42 @@
 //  Created by Олег Федоров on 08.10.2021.
 //
 
-import UIKit
+import RealmSwift
 
 class TaskListViewController: UITableViewController {
+    
+    private var taskLists: Results<TaskList>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         createTempData()
+        taskLists = StorageManager.shared.realm.objects(TaskList.self)
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        taskLists.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskListCell", for: indexPath)
-
+        let taskList = taskLists[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = taskList.name
+        content.secondaryText = "\(taskList.tasks.count)"
+        
+        cell.contentConfiguration = content
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        guard let tasksVC = segue.destination as? TasksViewController else { return }
+        let taskList = taskLists[indexPath.row]
+        tasksVC.taskList = taskList
     }
     
     @IBAction func addButtonPressed(_ sender: Any) {
--- TaskListRealm/ViewControllers/TasksViewController.swift
+++ TaskListRealm/ViewControllers/TasksViewController.swift
@@ -5,28 +5,46 @@
 //  Created by Олег Федоров on 08.10.2021.
 //
 
-import UIKit
+import RealmSwift
 
 class TasksViewController: UITableViewController {
+    
+    var taskList: TaskList!
+    
+    private var currentTasks: Results<Task>!
+    private var completedTasks: Results<Task>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = taskList.name
+        currentTasks = taskList.tasks.filter("isComplete = false")
+        completedTasks = taskList.tasks.filter("isComplete = true")
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        0
+        2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        section == 0 ? currentTasks.count : completedTasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        section == 0 ? "CURRENT TASKS" : "COMPLETED TASKS"
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TasksCell", for: indexPath)
-
+        let task = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = task.name
+        content.secondaryText = task.note
+        
+        cell.contentConfiguration = content
         return cell
     }
     
